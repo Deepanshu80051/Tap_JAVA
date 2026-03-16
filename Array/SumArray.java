7ac2cf84ecf383ec7420ae7d1eb96d23200ab704
Deepanshu shekhar tiwari <121624507+Deepanshu80051@users.noreply.github.com>
2026-03-16 22:51:27 +0530

    Add files via upload

---
+++ Array/SumArray.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Scanner;
+
+public class SumArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter element size");
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+      int sum=0;
+        for(int i=0;i<n;i++){
+            sum +=arr[i];
+        }
+        System.out.println(sum);
+
+    }
+}
